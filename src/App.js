2e87c4d88e8b23d7ad23a9c36a35ce0ea20fec29
livemehere <rhdxoals@gmail.com>
2022-07-02 20:53:45 +0900

    수정 테스트

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>티민의 github Actions CI/CD 파이프라인</h1>
+      <h1>티민의 github Actions CI/CD 파이프라인 수정사항도 반영되냐!</h1>
     </div>
   );
 }
